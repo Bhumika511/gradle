2a5add32cf41bcb033047fce7db18f0695b56986
Bhumika Siripireddy(UST <272816@ust.com>
2026-07-10 12:25:22 +0530

    added allure

--- src/test/java/com/sdet/builder/test/OrderTest.java
+++ src/test/java/com/sdet/builder/test/OrderTest.java
@@ -7,7 +7,6 @@ import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OrderTest {
